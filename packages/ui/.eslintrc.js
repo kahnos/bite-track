577b476fb740532f5734fca283230023b009d892
Daniel Diaz <daniel.diaz@agrotoken.io>
2024-07-07 01:11:46 -0300

    refactor: delete ui and web for now

--- packages/ui/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["@bite-track/eslint-config/react.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
